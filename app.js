721de5d535a2b79401337ffe7f6852e70d86e750
DESKTOP-KO32EAR\icimi <icimidemirag@gmail.com>
2022-08-22 12:24:02 +0300

    Added pages

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 
+//Template engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
-})
+    res.status(200).render('index', {
+        page_name: 'index'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about'
+    });
+});
 
 const port = 3000;
 app.listen(port, () => {
