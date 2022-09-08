c64e4d241d98ffd0017172c67bb289e8d524b02f
DESKTOP-KO32EAR\icimi <icimidemirag@gmail.com>
2022-09-08 15:43:37 +0300

    Added courses page

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,24 @@
 const Course = require("../models/Course");
 
+exports.getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find();
+
+    res.status(200).render("courses",{
+      courses,
+      page_name: "courses",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
 exports.createCourse = async (req, res) => {
   try {
-  const course = await Course.create(req.body);
+    const course = await Course.create(req.body);
 
     res.status(201).json({
       status: "success",
