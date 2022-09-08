622c94fdc026a4d9c8f8facb71360fcb9ad927ab
DESKTOP-KO32EAR\icimi <icimidemirag@gmail.com>
2022-09-08 16:26:25 +0300

    Added slug

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -16,6 +16,22 @@ exports.getAllCourses = async (req, res) => {
   }
 };
 
+exports.getCourse = async (req, res) => {
+  try {
+    const course = await Course.findOne({slug:req.params.slug});
+
+    res.status(200).render("course",{
+      course,
+      page_name: "courses",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
 exports.createCourse = async (req, res) => {
   try {
     const course = await Course.create(req.body);
--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
@@ -15,7 +16,16 @@ const courseSchema = new Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    slug:{
+        type: String,
+        unique: true
     }
+});
+
+courseSchema.pre('validate', function(next){
+    this.slug = slugify(this.name, {lower: true, strict: true});
+    next();
 });
 
 const Course = mongoose.model('Course', courseSchema);
