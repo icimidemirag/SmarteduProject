7920d3274bcd4f3b1ccd2ec3922c8615e5cc5934
DESKTOP-KO32EAR\icimi <icimidemirag@gmail.com>
2022-09-13 12:13:40 +0300

    Added categories

--- models/Course.js
+++ models/Course.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 const Schema = mongoose.Schema;
 
-const courseSchema = new Schema({
+const CourseSchema = new Schema({
     name: {
         type: String,
         unique: true,
@@ -20,13 +20,17 @@ const courseSchema = new Schema({
     slug:{
         type: String,
         unique: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
     }
 });
 
-courseSchema.pre('validate', function(next){
+CourseSchema.pre('validate', function(next){
     this.slug = slugify(this.name, {lower: true, strict: true});
     next();
 });
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model('Course', CourseSchema);
 module.exports = Course;
